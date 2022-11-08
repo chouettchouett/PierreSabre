9e1c67991d438303eb1eeb111ed171da1c373619
chouettchouett <114426961+chouettchouett@users.noreply.github.com>
2022-11-08 22:02:27 +0100

    Update HistoireTP4.java

--- src/histoire/HistoireTP4.java
+++ src/histoire/HistoireTP4.java
@@ -4,18 +4,18 @@ import personnages.*;
 public class HistoireTP4 {
 	
 	public static void main(String[] args) {
-		Humain romain = new Humain("Romain", "granite citron", 54);
-		romain.direBonjour();
-		romain.acheter("boisson", 12);
-		romain.boire();
-		romain.acheter("jeu", 2);
-		romain.acheter("kimono", 50);
+		Humain prof = new Humain("Prof", "kombucha", 54);
+		prof.direBonjour();
+		prof.acheter("boisson", 12);
+		prof.boire();
+		prof.acheter("jeu", 2);
+		prof.acheter("kimono", 50);
 		Commercant marco = new Commercant("Marco",20);
 		marco.direBonjour();
 		marco.seFaireExtorquer();
 		marco.recevoirArgent(15);
 		marco.boire();
-		Yakuza yaku = new Yakuza("Yaku le daltonien","whisky",30,"Warsong");
+		Yakuza yaku = new Yakuza("Yaku Le Noir","whisky",30,"Warsong");
 		yaku.direBonjour();
 		yaku.extorquer(marco);
 		Ronin roro = new Ronin("Roro","Scotch",60);
